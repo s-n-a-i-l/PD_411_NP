5ce5ae5c2910f08c992012596c9d67f5bddd7088
User <User@DESKTOP-QHG18FL>
2025-11-07 15:33:17 +0300

    SpinPrefix DONE

--- IPcalculator/IPcalculator/resource.h
+++ IPcalculator/IPcalculator/resource.h
@@ -6,8 +6,10 @@
 #define IDC_STATIC_IP_ADDRESS           1001
 #define IDC_STATIC_IP_MASK              1003
 #define IDC_IPADDRESS                   1004
-#define IDC_IPADDRESS2                  1005
 #define IDC_IPMASK                      1005
+#define IDC_EDIT_PREFIX                 1006
+#define IDC_SPIN1                       1007
+#define IDC_SPIN_PREFIX                 1007
 
 // Next default values for new objects
 // 
@@ -15,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
